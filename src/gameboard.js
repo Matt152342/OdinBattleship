9c6d8aa7b6825bdc89554ec0ab02980d38b6aa7b
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-07-20 10:21:42 +0200

    Created placeShip function to vertically place the ship and receiveAttack function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,25 @@
 class GameBoard {
     constructor(length) {
         this.length = length;
-        this.gameboard = Array.from({ length: 10 }, () => Array(10).fill());
+        this.gameboard = Array.from({ length: this.length }, () => Array(10).fill());
     }
 
-    
+    placeShip (startCoordinate, ship) { // startCoord and endCoord is going to be formatted as [x, y]
+        if (!ship || !startCoordinate) {
+            throw new Error("Please enter required details (start coordinate, end coordinate and ship object.");
+        }
+
+        const [x, y] = startCoordinate;
+        if (y + ship.length > this.length) { // Check if ship is on y-axis
+            throw new Error("Ship placement goes out of bounds.");
+        }
+
+        for (let i = y; i < ship.length + y; i++) {
+            this.gameboard[x][i] = "SHIP";
+        }
+    }
+
+    receiveAttack (coordinate) {
+        gameboard[coordinate[0]][coordinate[1]] = "X";
+    }
 }
